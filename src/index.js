785703a324978139b666d413396fa969851d4ea5
Dalton Johnson <dalton@pendo.io>
2018-04-15 18:48:56 -0400

    simplify the logic

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 const handler = {
     get (_, name) {
-        return (...params) => {
-            let value = params.shift();
-            if (params.length && value && typeof value === 'object') {
-                value = params.reduce(
-                    (acc, value) => value && typeof value === 'object'
-                        ? Object.assign(acc, value)
-                        : acc,
-                value);
-            }
-
-            return {[name]: value};
+        return (value, ...rest) => {
+            return {
+                [name]: rest.length && value != null
+                    ? Object.assign(value, ...rest)
+                    : value
+            };
         };
     }
 };
